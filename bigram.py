8c486428949da2d2d108abd1b6cdd3fe4b340516
Sanjay Patel <sjp123@gmail.com>
2024-02-28 14:07:04 -0600

    Another update

--- bigram.py
+++ bigram.py
@@ -107,21 +107,26 @@ print("Starting Training Loop")
 # create a PyTorch optimizer
 optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
 
-# for iter in range(max_iters):
+for iter in range(max_iters):
 
-#     # every once in a while evaluate the loss on train and val sets
-#     if iter % eval_interval == 0:
-#         losses = estimate_loss()
-#         print(f"step {iter}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}")
+    # every once in a while evaluate the loss on train and val sets
+    if iter % eval_interval == 0:
+        losses = estimate_loss()
+        print(f"step {iter}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}")
 
-#     # sample a batch of data
-#     xb, yb = get_batch('train')
+    # sample a batch of data\
+    print("Getting batch")
+    xb, yb = get_batch('train')
 
 #     # evaluate the loss
-#     logits, loss = model(xb, yb)
-#     optimizer.zero_grad(set_to_none=True)
-#     loss.backward()
-#     optimizer.step()
+    print("Calculating loss")
+    logits, loss = model(xb, yb)
+    print("Zeroing grads")
+    optimizer.zero_grad(set_to_none=True)
+    print("Backprop")
+    loss.backward()
+    print("Step")
+    optimizer.step()
 
 # generate from the model
 context = torch.zeros((1, 1), dtype=torch.long, device=device)
