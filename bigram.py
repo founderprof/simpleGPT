f06db79ad1fe7b5a5e46dc93b965e108d0827a57
Sanjay Patel <sjp123@gmail.com>
2024-02-28 14:40:53 -0600

    Another

--- bigram.py
+++ bigram.py
@@ -41,9 +41,13 @@ val_data = data[n:]
 def get_batch(split):
     # generate a small batch of data of inputs x and targets y
     data = train_data if split == 'train' else val_data
+    print("Torch randint")
     ix = torch.randint(len(data) - block_size, (batch_size,))
+    print("Torch stack 1")
     x = torch.stack([data[i:i+block_size] for i in ix])
+    print("Torch stack 2")
     y = torch.stack([data[i+1:i+block_size+1] for i in ix])
+    print("Push data to device")
     x, y = x.to(device), y.to(device)
     return x, y
 
