8e6003747318716df1f32aeb898be95823011dbd
Sanjay Patel <39928604+founderprof@users.noreply.github.com>
2024-02-28 13:27:00 -0600

    Update bigram.py

--- bigram.py
+++ bigram.py
@@ -112,7 +112,7 @@ optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
 #    # every once in a while evaluate the loss on train and val sets
 #    if iter % eval_interval == 0:
 #        losses = estimate_loss()
-        print(f"step {iter}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}")
+#        print(f"step {iter}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}")
 #
 #    # sample a batch of data
     xb, yb = get_batch('train')
