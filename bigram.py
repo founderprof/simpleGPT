c13338e758b1b08ed83d31f446c4539791342bf7
Sanjay Patel <sjp123@gmail.com>
2024-02-29 10:44:49 -0600

    Simple update

--- bigram.py
+++ bigram.py
@@ -6,8 +6,8 @@ from torch.nn import functional as F
 # hyperparameters
 batch_size = 32 # how many independent sequences will we process in parallel?
 block_size = 8 # what is the maximum context length for predictions?
-max_iters = 5
-eval_interval = 1
+max_iters = 10
+eval_interval = 5
 learning_rate = 1e-2
 # device = 'cuda' if torch.cuda.is_available() else 'cpu'
 device = 'cpu'
@@ -41,13 +41,13 @@ val_data = data[n:]
 def get_batch(split):
     # generate a small batch of data of inputs x and targets y
     data = train_data if split == 'train' else val_data
-    print("Torch randint", flush=True)
+ #   print("Torch randint", flush=True)
     ix = torch.randint(len(data) - block_size, (batch_size,))
-    print("Torch stack 1", flush=True)
+ #   print("Torch stack 1", flush=True)
     x = torch.stack([data[i:i+block_size] for i in ix])
-    print("Torch stack 2", flush=True)
+ #   print("Torch stack 2", flush=True)
     y = torch.stack([data[i+1:i+block_size+1] for i in ix])
-    print("Push data to device", flush=True)
+ #   print("Push data to device", flush=True)
     x, y = x.to(device), y.to(device)
     return x, y
 
@@ -116,7 +116,7 @@ print("Optimizer setup done", flush=True)
 for iter in range(max_iters):
     print(f"Iteration {iter}", flush=True)
     # every once in a while evaluate the loss on train and val sets
-    if iter % eval_interval == 0:
+    if (iter > 0) and (iter % eval_interval == 0):
         losses = estimate_loss()
         print(f"step {iter}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}", flush=True)
 
